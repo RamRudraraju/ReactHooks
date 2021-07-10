90e05b61e2d8fd4499a0bfdeeb513c1e4d61dc63
Rama krishna Raju Rudraraju <rrkr2016@uab.edu>
2021-07-09 20:58:12 -0500

    Regular Variable & Obj in setState

---
+++ setStateHook/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import TwoCountersOption1 from './TwoCountersOption2';
+import TwoCountersOption2 from './TwoCountersOption2';
+
+class App extends Component {
+  render() {
+    return <div style={{margin:'10px'}}>
+      <TwoCountersOption2/>
+    </div>;
+  }
+}
+
+export default App;
